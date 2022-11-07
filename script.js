c578ea2fdd03a5ddfeb39cece0a6928b74b48402
DanieleAntonaci <danieleanto96@gmail.com>
2022-11-07 14:55:33 +0100

    Inizio creazione file css e correzione js

--- script.js
+++ script.js
@@ -10,19 +10,18 @@ console.log(kmDaPercorrere, etaPasseggero);
 
 // calcolare il prezzo 0,21 € 
 let prezzoViaggio = kmDaPercorrere * 0.21;
-let prezzoScontatoViaggio
+let prezzoScontatoViaggio;
 
 // se hanno un eta differente cambia il prezzo e stampa il risultato
 if (etaPasseggero < 18) {
     prezzoScontatoViaggio = (prezzoViaggio - (prezzoViaggio * 20 / 100)).toFixed(2);
-    document.getElementById('prezzo_viaggio').innerHTML = `Salve, per percorrere ${kmDaPercorrere}km, coniderata la sua et&aacute; di ${etaPasseggero} anni, il prezzo del biglietto sar&aacute; di ${prezzoScontatoViaggio}&euro;`;
 
 } else if (etaPasseggero > 65) {
     prezzoScontatoViaggio = (prezzoViaggio - (prezzoViaggio * 40 / 100)).toFixed(2);
-    document.getElementById('prezzo_viaggio').innerHTML = `Salve, per percorrere ${kmDaPercorrere}km, coniderata la sua et&aacute; di ${etaPasseggero} anni, il prezzo del biglietto sar&aacute; di ${prezzoScontatoViaggio}&euro;`;
 
 } else {
-    document.getElementById('prezzo_viaggio').innerHTML = `Salve, per percorrere ${kmDaPercorrere}km, coniderata la sua et&aacute; di ${etaPasseggero} anni, il prezzo del biglietto sar&aacute; di ${prezzoViaggio}&euro;`;
 };
 
+// messaggio di risposta alla richiesta
+document.getElementById('prezzo_viaggio').innerHTML = `Salve, per percorrere ${kmDaPercorrere}km, coniderata la sua et&aacute; di ${etaPasseggero} anni, il prezzo del biglietto sar&aacute; di ${prezzoScontatoViaggio || prezzoViaggio}&euro;`;
 
